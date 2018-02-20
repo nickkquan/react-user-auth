616c17863bd3da0df4a9f24ad6f0a4b8979075bc
Nick Quan <nickkquan@gmail.com>
2018-02-20 14:32:37 -0800

    persistent login completed

--- src/components/app.js
+++ src/components/app.js
@@ -10,6 +10,7 @@ import Quote from "./quote";
 import SignIn from "./sign-in";
 import SignUp from "./sign-up";
 import auth from "../hoc/auth";
+import redirect from "../hoc/redirect";
 
 const App = () => (
 	<div className="container">
@@ -17,8 +18,8 @@ const App = () => (
 		<Route exact path="/" component={Home} />
 		<Route path="/about-us" component={About} />
 		<Route path="/movie-quote" component={auth(Quote)} />
-		<Route path="/sign-in" component={SignIn} />
-		<Route path="/sign-up" component={SignUp} />
+		<Route path="/sign-in" component={redirect(SignIn, "/movie-quote")} />
+		<Route path="/sign-up" component={redirect(SignUp, "/movie-quote")} />
 	</div>
 );
 
